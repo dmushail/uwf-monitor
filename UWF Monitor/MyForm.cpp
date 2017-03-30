e775e7c85cb2272f19aa27f3d384b775a79791dc
dmushail <dmushail@gmail.com>
2017-03-30 11:46:53 -0400

    Initial Upload

---
+++ UWF Monitor/MyForm.cpp
@@ -0,0 +1,135 @@
+#include "MyForm.h"
+#include "resource.h"
+#include <Windows.h>
+#include <string>
+
+
+using namespace Microsoft::Win32;
+using namespace System;
+using namespace System::Windows::Forms;
+
+
+//Gets UWF Status from Registry
+void getUWF(int& c, int& n)
+{
+	int current;
+	int next;
+	HKEY hKey;
+	DWORD value_length = sizeof(DWORD);
+
+	if (RegOpenKey(HKEY_LOCAL_MACHINE, TEXT("SYSTEM\\ControlSet001\\Services\\uwfvol\\Parameters\\Static\\"), &hKey) != ERROR_SUCCESS)
+	{
+		MessageBox::Show("Unable to open registry key!", "UWF Manager", MessageBoxButtons::OK, MessageBoxIcon::Error);
+	}
+
+	RegQueryValueEx(hKey, TEXT("CurrentSettings"), NULL, NULL, (LPBYTE)&current, &value_length);
+	RegQueryValueEx(hKey, TEXT("UpdatedSettings"), NULL, NULL, (LPBYTE)&next, &value_length);
+
+	c = current;
+	n = next;
+
+	RegCloseKey(hKey);
+	
+}
+
+
+//Get UWF status and return corresponding tray icon.
+HICON loadIcon()
+{
+	HINSTANCE hInstance = GetModuleHandle(NULL);
+	HICON Green = LoadIcon(hInstance, MAKEINTRESOURCE(IDI_ICON1));
+	HICON Orange = LoadIcon(hInstance, MAKEINTRESOURCE(IDI_ICON2));
+	HICON Red = LoadIcon(hInstance, MAKEINTRESOURCE(IDI_ICON3));
+
+	int current, next;
+
+	getUWF(current, next);
+
+	if (current == 0 && current == next)
+	{
+		return Red;
+	}
+	else if (current == 1 && current == next)
+	{
+		return Green;
+	}
+	else
+	{
+		return Orange;
+	}
+	return 0;
+}
+
+
+//Launch UWF Manager GUI.
+int loadMgr()
+{
+
+	SHELLEXECUTEINFO ShExecInfo = { 0 };
+	ShExecInfo.cbSize = sizeof(SHELLEXECUTEINFO);
+	ShExecInfo.fMask = SEE_MASK_NOCLOSEPROCESS;
+	ShExecInfo.hwnd = NULL;
+	ShExecInfo.lpVerb = NULL;
+	ShExecInfo.lpFile = L"C:\\Program Files\\UWF Manager\\uwfgui.exe";
+	ShExecInfo.lpParameters = NULL;
+	ShExecInfo.lpDirectory = NULL;
+	ShExecInfo.nShow = SW_SHOW;
+	ShExecInfo.hInstApp = NULL;
+
+	if (ShellExecuteEx(&ShExecInfo))
+	{
+		WaitForSingleObject(ShExecInfo.hProcess, INFINITE);
+		return 0;
+	}
+	else
+	{
+		return 1;
+	}
+}
+
+
+// Check if startup item exists, if not, ask the user if they would like to add.
+void checkStartup()
+{
+	HKEY hKey;
+
+	if (RegOpenKeyEx(HKEY_LOCAL_MACHINE, TEXT("SOFTWARE\\Microsoft\\Windows\\CurrentVersion\\Run\\"), 0, KEY_ALL_ACCESS | KEY_WOW64_64KEY, &hKey) != ERROR_SUCCESS)
+	{
+		MessageBox::Show("Unable to open configuration key!", "UWF Manager", MessageBoxButtons::OK, MessageBoxIcon::Error);
+	}
+	else
+	{
+		DWORD value_length = sizeof(DWORD);
+		if (RegQueryValueEx(hKey, TEXT("UWF_Monitor"), NULL, NULL, NULL, &value_length) != ERROR_SUCCESS)
+		{
+			if (MessageBox::Show("Would you like to add the UWF Monitor to system startup?", "UWF Monitor", MessageBoxButtons::YesNo, MessageBoxIcon::Information) == DialogResult::Yes)
+			{
+				std::wstring path = TEXT("C:\\Program Files\\UWF Manager\\uwfmon.exe");
+				int pathLength = path.length();
+				if (RegSetValueEx(hKey, TEXT("UWF_Monitor"), 0, REG_SZ, (LPBYTE)path.c_str(), (path.size() +1)* sizeof(wchar_t)) != ERROR_SUCCESS)
+				{
+					MessageBox::Show("Unable to add UWF Monitor to startup, make sure you are running as administrator.", "UWF Monitor", MessageBoxButtons::OK, MessageBoxIcon::Error);
+				}
+				else
+				{
+					RegCloseKey(hKey);
+					MessageBox::Show("Successfully added to startup! Please make sure this executable is renamed and copied to \"C:\\Program Files\\UWF Manager\\uwfmon.exe\"", "UWF Monitor", MessageBoxButtons::OK, MessageBoxIcon::Information);
+				}
+			}
+		}
+	}
+}
+
+
+[STAThread]
+void Main(array<String^>^ args)
+{
+	Application::EnableVisualStyles();
+	Application::SetCompatibleTextRenderingDefault(false);
+
+	checkStartup();
+
+	UWFMonitor::MyForm form;
+	Application::Run(%form);
+}
+
---
+++ UWF Monitor/MyForm.h
@@ -0,0 +1,119 @@
+#include "resource.h"
+#include <Windows.h>
+#pragma once
+
+
+int loadMgr();
+HICON loadIcon();
+
+namespace UWFMonitor {
+
+	using namespace System;
+	using namespace System::ComponentModel;
+	using namespace System::Collections;
+	using namespace System::Windows::Forms;
+	using namespace System::Data;
+	using namespace System::Drawing;
+
+	/// <summary>
+	/// Summary for MyForm
+	/// </summary>
+	public ref class MyForm : public System::Windows::Forms::Form
+	{
+	public:
+		MyForm(void)
+		{
+			InitializeComponent();
+			//
+			//TODO: Add the constructor code here
+			//
+		}
+
+	protected:
+		/// <summary>
+		/// Clean up any resources being used.
+		/// </summary>
+		~MyForm()
+		{
+			if (components)
+			{
+				delete components;
+			}
+		}
+	private: System::Windows::Forms::NotifyIcon^  uwfMon;
+
+
+
+
+	protected:
+
+	protected:
+	private: System::ComponentModel::IContainer^  components;
+
+	private:
+		/// <summary>
+		/// Required designer variable.
+		/// </summary>
+
+
+#pragma region Windows Form Designer generated code
+		/// <summary>
+		/// Required method for Designer support - do not modify
+		/// the contents of this method with the code editor.
+		/// </summary>
+		void InitializeComponent(void)
+		{
+			this->components = (gcnew System::ComponentModel::Container());
+			System::ComponentModel::ComponentResourceManager^  resources = (gcnew System::ComponentModel::ComponentResourceManager(MyForm::typeid));
+			this->uwfMon = (gcnew System::Windows::Forms::NotifyIcon(this->components));
+			this->SuspendLayout();
+			// 
+			// uwfMon
+			// 
+			this->uwfMon->Icon = (cli::safe_cast<System::Drawing::Icon^>(resources->GetObject(L"uwfMon.Icon")));
+			this->uwfMon->Text = L"Write Filter";
+			this->uwfMon->Visible = true;
+			this->uwfMon->DoubleClick += gcnew System::EventHandler(this, &MyForm::uwfMon_DoubleClick);
+			// 
+			// MyForm
+			// 
+			this->AutoScaleDimensions = System::Drawing::SizeF(6, 13);
+			this->AutoScaleMode = System::Windows::Forms::AutoScaleMode::Font;
+			this->AutoValidate = System::Windows::Forms::AutoValidate::Disable;
+			this->ClientSize = System::Drawing::Size(10, 10);
+			this->ControlBox = false;
+			this->Cursor = System::Windows::Forms::Cursors::Default;
+			this->Enabled = false;
+			this->FormBorderStyle = System::Windows::Forms::FormBorderStyle::None;
+			this->Name = L"MyForm";
+			this->Opacity = 0;
+			this->ShowIcon = false;
+			this->ShowInTaskbar = false;
+			this->SizeGripStyle = System::Windows::Forms::SizeGripStyle::Hide;
+			this->Text = L"MyForm";
+			this->WindowState = System::Windows::Forms::FormWindowState::Minimized;
+			this->Load += gcnew System::EventHandler(this, &MyForm::MyForm_Load);
+			this->ResumeLayout(false);
+
+		}
+#pragma endregion
+	private: System::Void uwfMon_DoubleClick(System::Object^  sender, System::EventArgs^  e) {
+
+
+				 // Uncomment loadMgr below if you would like to launch UWF Manager on doubleclick.
+				 // Make sure the UWF Monitor executable is copied to and renamed to match the execution
+				 // path within the loadMgr function.
+				  
+				 //loadMgr();
+
+				 //Refreshes status indicator on doubleclick.
+				 System::Drawing::Icon^ newIco = System::Drawing::Icon::FromHandle((IntPtr)loadIcon());
+				 this->uwfMon->Icon = newIco;
+	}
+	private: System::Void MyForm_Load(System::Object^  sender, System::EventArgs^  e) {
+
+				 System::Drawing::Icon^ newIco = System::Drawing::Icon::FromHandle((IntPtr)loadIcon());
+				 this->uwfMon->Icon = newIco;
+	}
+	};
+}
